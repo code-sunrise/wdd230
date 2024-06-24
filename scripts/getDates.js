3d8760b88f2ebab856e73f9d57d8d9973e8c651e
code-sunrise <145300741+code-sunrise@users.noreply.github.com>
2024-06-24 12:10:39 -0400

    last mod update

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,8 +1,26 @@
 
-let lastMod = null;
-
-function updateLastMod() {
-    lastMod = new Date();
+function getThisYear() {
+    return new Date().getFullYear();
 }
 
-updateLastMod
+function lastModDate() {
+    return new Date(document.lastModified);
+}
+
+function update() {
+    const year = document.getElementById('year');
+    if (year) {
+        year.textContent = getThisYear();
+    }
+}
+
+function updateLastMod() {
+    const lastModSpan = document.getElementById('lastModified');
+    if (lastModSpan) {
+        const lastMod = lastModDate();
+        lastMod.textContent = "Last Modified: " + lastMod.toLocalString();
+    }
+}
+
+update();
+updateLastMod();
