01e288d145c7dd080848baed6e0fe5d806251e50
code-sunrise <145300741+code-sunrise@users.noreply.github.com>
2024-06-25 17:06:47 -0400

    dom activity done

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,5 +1,22 @@
 // declare variables
 
-const input = document.querySelector('#favChap');
+const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
+
+//event listener. Arrow function => 
+//what is listening?
+//button?
+button.addEventListener('click', () => {
+    if (input.value != '') {
+        const li = document.createElement('li');
+        const closeButton = document.createElement('button');
+        li.innerText = input.value;
+        closeButton.textContent = '❌';
+        li.append(closeButton); //?? is this right?
+        list.append(li);
+        closeButton.addEventListener('click', () => { list.removeChild(li);});
+        input.value = '';
+        input.focus();
+    }
+})
